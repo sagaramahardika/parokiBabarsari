1db2d1e7832782ed1db885b2fe6db4c6a0bbc7e0
Ervan Kurnia Sanjaya <ervan.sanjaya@ti.ukdw.ac.id>
2016-10-29 23:04:30 +0700

    Clear Database Pernikahans

--- sql/pernikahans.sql
+++ sql/pernikahans.sql
@@ -3,7 +3,7 @@
 -- http://www.phpmyadmin.net
 --
 -- Host: 127.0.0.1
--- Generation Time: Oct 05, 2016 at 08:12 AM
+-- Generation Time: Oct 29, 2016 at 06:04 PM
 -- Server version: 5.6.21
 -- PHP Version: 5.6.3
 
@@ -46,17 +46,7 @@ CREATE TABLE IF NOT EXISTS `pernikahans` (
   `saksi2` varchar(100) DEFAULT NULL,
   `dispensasi` varchar(255) DEFAULT NULL,
   `romopernikahan` varchar(150) NOT NULL
-) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=latin1;
-
---
--- Dumping data for table `pernikahans`
---
-
-INSERT INTO `pernikahans` (`id`, `kode_stasi`, `kode_LM`, `no`, `halaman`, `umat_id`, `pasangan_id`, `nm_pasangan`, `alamat_pasangan`, `telp_pasangan`, `tmplhr_pasangan`, `tgllhr_pasangan`, `tmppernikahan`, `tglpernikahan`, `kota`, `saksi1`, `saksi2`, `dispensasi`, `romopernikahan`) VALUES
-(1, 1, 1, 1, 1, 487, NULL, 'PRATIWI', 'PRATIWI', 'PRATIWI', 'PRATIWI', '2016-09-06', 'PRATIWI', '2016-09-06', 'PRATIWI', 'PRATIWI', 'PRATIWI', 'PRATIWI', 'PRATIWI'),
-(2, 2, 2, 2, 2, 490, 156, NULL, NULL, NULL, NULL, NULL, 'Kudus', '2016-09-25', 'Kudus', 'Kudus', 'Kudus', 'Kudus', 'Kudus'),
-(3, 1, 2, 3, 4, 487, 30, NULL, NULL, NULL, NULL, NULL, 'Jogja', '2016-09-07', 'Jogja', 'Aditya', 'Yuga', 'Pradana', 'Sagara'),
-(4, 3776, 1, 2, 3, 491, 9, NULL, NULL, NULL, NULL, NULL, 'awd', '2016-10-05', 'awdaw', 'ad', 'awawd', 'awdawd', 'dawd');
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
 -- Indexes for dumped tables
@@ -76,7 +66,7 @@ ALTER TABLE `pernikahans`
 -- AUTO_INCREMENT for table `pernikahans`
 --
 ALTER TABLE `pernikahans`
-MODIFY `id` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=5;
+MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
